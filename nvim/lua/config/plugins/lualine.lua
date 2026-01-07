3fb88b9b7d6b3768782165beb321ae78adb477ec
szbmrk <szmark16@gmail.com>
2026-01-07 18:10:28 +0100

    .

--- nvim/lua/config/plugins/lualine.lua
+++ nvim/lua/config/plugins/lualine.lua
@@ -2,12 +2,4 @@ return {
     'nvim-lualine/lualine.nvim',
     as = 'lualine',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require(
-            "lualine"
-        ).setup({
-            theme = "rose-pine",
-        })
-    end
 }
-
--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,51 +1,51 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		local ts = require("nvim-treesitter")
-		local parsers = {
-			"bash",
-			"c",
-			"c_sharp",
-			"diff",
-			"html",
-			"javascript",
-			"jsdoc",
-			"json",
-			"jsonc",
-			"lua",
-			"luadoc",
-			"luap",
-			"markdown",
-			"markdown_inline",
-			"printf",
-			"python",
-			"query",
-			"rust",
-			"regex",
-			"toml",
-			"tsx",
-			"typescript",
-			"vim",
-			"vimdoc",
-			"xml",
-			"yaml",
-			"go",
-			"gdscript",
-			"godot_resource",
-			"gdshader",
-		}
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local ts = require("nvim-treesitter")
+        local parsers = {
+            "bash",
+            "c",
+            "c_sharp",
+            "diff",
+            "html",
+            "javascript",
+            "jsdoc",
+            "json",
+            "jsonc",
+            "lua",
+            "luadoc",
+            "luap",
+            "markdown",
+            "markdown_inline",
+            "printf",
+            "python",
+            "query",
+            "rust",
+            "regex",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "xml",
+            "yaml",
+            "go",
+            "gdscript",
+            "godot_resource",
+            "gdshader",
+        }
 
-		for _, parser in ipairs(parsers) do
-			ts.install(parser)
-		end
+        for _, parser in ipairs(parsers) do
+            ts.install(parser)
+        end
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = parsers,
-			callback = function()
-				vim.treesitter.start()
-			end,
-		})
-	end,
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = parsers,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
