840f09b85349b0781457999b556ecaa5b4be6527
szbmrk <szmark16@gmail.com>
2026-06-06 11:07:20 +0200

    .

--- hypr/conf/environment.lua
+++ hypr/conf/environment.lua
@@ -4,6 +4,8 @@ hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
 hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("HYPRCURSOR_SIZE", "24")
 
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
